c6cae26326d176173f68ce3cebdc80f8cd90c52e
rational-architecture-1641 <jarmah241@gmail.com>
2020-10-07 13:18:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ end
 
 def get_japanese_emoticon(file, emoticon)
   emote_lib = load_library(file)
-  p emote_lib[emoticon]
+  p emote_lib[emoticon][:japanese]
 end
 
 def get_english_meaning(file, emoticon)
