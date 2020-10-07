9d2edc154bed5daac895577e93b73e3f0ea9e6b9
rational-architecture-1641 <jarmah241@gmail.com>
2020-10-07 13:07:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,11 +5,12 @@ def load_library(file)
   # code goes here
   emote_library = YAML.load_file(file)
   emote_hash = {}
-  
-  emote_library.each do 
-  
-  
-  emote_library
+  emote_library.each do |emote|
+    emote_hash[emote[0]] = {
+      :english => emote[1][0],
+      :japanese => emote[1][1]
+    }
+  end
 end
 
 def get_japanese_emoticon
